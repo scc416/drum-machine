664be25e00c19fee5a4c9f892e50569ae79c8489
Siu <siuc@protonmail.com>
2021-12-26 17:14:29 -0500

    add reducer

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -11,7 +11,37 @@ import {
 
 const useData = () => {
   const reducers = {
-    
+    [PLAY]: (state, { key }) => {
+      return { ...state, key, class: true };
+    },
+    [CLASS_CHANGE]: (state) => {
+      return { ...state, class: false };
+    },
+    [ENDED]: (state) => {
+      return { ...state, class: false, key: null };
+    },
+    [POWER]: (state) => {
+      return { ...state, on: !state.on };
+    },
+    [VOLUME_START]: (state, { newVolume }) => {
+      return {
+        ...state,
+        volumeChanging: true,
+        volume: newVolume,
+        muted: false,
+      };
+    },
+    [VOLUME_END]: (state, { newVolume }) => {
+      return {
+        ...state,
+        volumeChanging: false,
+        volume: newVolume,
+        muted: false,
+      };
+    },
+    [MUTE]: (state) => {
+      return { state, muted: !state.muted};
+    },
   };
 
   const reducer = (state, action) => {
@@ -26,6 +56,8 @@ const useData = () => {
     volumeChanging: false,
     muted: false,
   });
+
+  return {};
 };
 
 export default useData;
