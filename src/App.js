53953ba72b0ac5bafdd1340e3c767a30c80a0188
Siu <siuc@protonmail.com>
2021-12-26 16:57:33 -0500

    moving html to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { soundLinks } from "./constants";
 
-function App() {
+const padElement = (key) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id={"pad-" + key}
+      className="drum-pad clickable"
+      onClick={() =>console.log("CLICK")
+        // this.props.play(key, this.props.on, this.props.volume, this.props.muted)
+      }
+      // style={playingStyle(key)}
+    >
+      <audio
+        id={key}
+        className="clip"
+        onTimeUpdate={ () => console.log("onTImeupdate")
+          // (event) =>
+          // this.props.classChange(
+          //   event.target.currentTime,
+          //   key,
+          //   this.props.state
+          // )
+        }
+        onEnded={() => console.log("HI")
+          // this.props.ending(key, this.props.state)
+        }
+        src={soundLinks[key]}
+      />
+      {key}
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div
+      onMouseUp={
+        () => console.log("MOUSE UP")
+        // (e) => this.props.volumeEnd(e.clientX, this.props.volumeChanging)
+      }
+      onMouseMove={
+        () => console.log("MOUSE UP")
+        // (e) => this.props.volumeStart(e.clientX, this.props.volumeChanging)
+      }
+      tabIndex="0"
+      id="drum-machine"
+      onKeyPress={
+        () => console.log("onkeypress")
+        // (e) =>
+        // this.props.play(
+        //   e.key.toUpperCase(),
+        //   this.props.on,
+        //   this.props.volume,
+        //   this.props.muted
+        // )
+      }
+    >
+      <div className="player">
+        <div id="display">
+          <div 
+          // style={hideScreen(this.props.on)}
+            >
+            <span className="title">Drums and Percussion</span>
+            <div className="show">
+              {/* {instrument[this.props.state]} */}
+              </div>
+            <div className="control">
+              <div className="volume">
+                <div
+                  // onMouseDown={this.props.mute}
+                  className="clickable to-be-clicked"
+                >
+                  <i
+                    // className={volumeIcon(this.props.volume, this.props.muted)}
+                  />
+                </div>
+
+                <div
+                  className="volume-bar clickable"
+                  // onMouseDown={(e) => this.props.volumeStart(e.clientX, true)}
+                >
+                  <div id="volume-bar-background" />
+                  <div
+                    className="drag-bar"
+                    // style={barPosition(this.props.volume, this.props.muted)}
+                  >
+                    <div
+                      id="volume-bar"
+                      // style={volumeWidth(this.props.volume, this.props.muted)}
+                    >
+                      <div id="drag" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div
+                id="power"
+                // style={powerStyle(this.props.on)}
+                // onClick={this.props.power}
+                className="clickable"
+              >
+                <i class="fas fa-power-off" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="circle-container">
+          {padElement("D")}
+          {padElement("C")}
+          {padElement("X")}
+          {padElement("Z")}
+          {padElement("A")}
+          {padElement("Q")}
+          {padElement("W")}
+          {padElement("E")}
+          {padElement("S")}
+          <div className="circle" />
+        </div>
+      </div>
+      <div className="footer">
+        Design cloned from&nbsp;
+        <a
+          href="https://www.csaimages.com/preview.asp?image=837422&itemw=4&itemf=0001&itemstep=1&itemx=2"
+          target="_blank"
+        >
+          CSA IMAGES
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const soundLinks = {
+  Q: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-free-to-use-sounds/ftus_instrument_drum_large_gamelan_hit_single_001_472.mp3",
+  W: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-free-to-use-sounds/ftus_instrument_drum_small_gamelan_hit_stick_single_002_478.mp3",
+  E: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-14566/zapsplat_musical_cymbal_crash_tap_light_thin_stick_001_17604.mp3",
+  A: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-free-to-use-sounds/ftus_musical_instrument_gender_gamelan_single_mallet_hit_mid_low_004_502.mp3",
+  S: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-free-to-use-sounds/ftus_musical_instrument_gender_gamelan_single_mallet_hit_high_mid_001_489.mp3",
+  D: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-46416/zapsplat_musical_triangle_single_strike_002_48598.mp3",
+  Z: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-55112/zapsplat_musical_perscussion_shaker_maracas_kids_single_shake_002_61893.mp3",
+  X: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-55112/zapsplat_musical_perscussion_tambourine_kids_set_down_61898.mp3",
+  C: "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-free-to-use-sounds/ftus_musical_instrument_ching_gamelan_single_hit_001_529.mp3"
+};
+
