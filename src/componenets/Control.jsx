88ffc64e14c047189c01baf2933122420bfe4c97
Siu <siuc@protonmail.com>
2021-12-26 20:20:46 -0500

    separate component Control from Display

---
+++ src/componenets/Control.jsx
@@ -0,0 +1,37 @@
+import Power from "./Power";
+import { VOLUME_START } from "../constants";
+
+const Control = (props) => {
+  const {
+    volumeIcon,
+    volumeFunc,
+    barPosition,
+    volumeWidth,
+    mute,
+    powerStyle,
+    power,
+  } = props;
+  return (
+    <div className="control">
+      <div className="volume">
+        <div onMouseDown={mute} className="clickable to-be-clicked">
+          <i className={volumeIcon()} />
+        </div>
+        <div
+          className="volume-bar clickable"
+          onMouseDown={volumeFunc(VOLUME_START)}
+        >
+          <div id="volume-bar-background" />
+          <div className="drag-bar" style={barPosition()}>
+            <div id="volume-bar" style={volumeWidth()}>
+              <div id="drag" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Power {...{ power, powerStyle }} />
+    </div>
+  );
+};
+
+export default Control;
--- src/componenets/Display.jsx
+++ src/componenets/Display.jsx
@@ -1,42 +1,13 @@
-import { VOLUME_START } from "../constants";
-import Power from "./Power";
+import Control from "./Control";
 
-const Display = ({
-  volumeIcon,
-  volumeFunc,
-  barPosition,
-  volumeWidth,
-  hideScreen,
-  currentInstument,
-  mute,
-  powerStyle,
-  power,
-}) => {
+const Display = (props) => {
+  const { hideScreen, currentInstument } = props;
   return (
     <div id="display">
       <div style={hideScreen}>
         <span className="title">Drums and Percussion</span>
         <div className="show">{currentInstument}</div>
-        <div className="control">
-          <div className="volume">
-            <div onMouseDown={mute} className="clickable to-be-clicked">
-              <i className={volumeIcon()} />
-            </div>
-
-            <div
-              className="volume-bar clickable"
-              onMouseDown={volumeFunc(VOLUME_START)}
-            >
-              <div id="volume-bar-background" />
-              <div className="drag-bar" style={barPosition()}>
-                <div id="volume-bar" style={volumeWidth()}>
-                  <div id="drag" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <Power {...{ power, powerStyle }} />
-        </div>
+        <Control {...props} />
       </div>
     </div>
   );
