1e4648af5b68c8b0b88d51a8c9284a824fc76cec
Siu <siuc@protonmail.com>
2021-12-26 21:03:06 -0500

    turn id to class

--- src/componenets/Display.jsx
+++ src/componenets/Display.jsx
@@ -3,7 +3,7 @@ import Control from "./Control";
 const Display = (props) => {
   const { hideScreen, currentInstument } = props;
   return (
-    <div id="display">
+    <div className="display">
       <div style={hideScreen}>
         <span className="title">Drums and Percussion</span>
         <div className="show">{currentInstument}</div>
